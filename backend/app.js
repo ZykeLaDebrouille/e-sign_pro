8b5a3320e2d83835993532198f755104492c939e
zykeladebrouille <gemicieneshakki@gmail.com>
2025-02-20 14:20:10 +0100

    Fix: resolve merge conflicts in app.js and restore original route handling

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,9 @@ const cors = require('cors');
 const db = require('./db');
 const usersRoutes = require('./routes/users');
 const documentsRoutes = require('./routes/documents');
-<<<<<<< Updated upstream
-const PORT = process.env.PORT || 5000;
-=======
-
 const setupSwagger = require("./swagger");
+const PORT = process.env.PORT || 5000;
 
->>>>>>> Stashed changes
 
 const app = express();
 app.use(cors());
@@ -23,13 +19,6 @@ app.use('/api/users', usersRoutes);
 app.use('/api/documents', documentsRoutes);
 
 
-<<<<<<< Updated upstream
-app.get('/', (req, res) => {
-  res.send('ON EST DANS LE ROUTE HOME!')
-  });
-
-=======
->>>>>>> Stashed changes
 app.get('/api/hello', (req, res) => {
   res.send('Hello from /API/HELLO Backend!');
 });
